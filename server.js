3512a9a22a62c806f0fb60f5ffff15f250180f21
Jeff Cela <cela.j@labrador-company.com>
2021-09-14 23:25:01 -0400

    fixed bugs in server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
-const db = require("./db/db.json");
+const db = require("./Public/db/db.json");
 const fs = require("fs");
 const path = require("path");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("Public"));
+app.use(express.static("public"));
 
 const PORT = process.env.PORT || 3001;
 
